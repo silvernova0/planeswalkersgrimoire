a580faf7cc83d1a8db5665f76398b6f97d26c6f1
social <silvernova0@gmail.com>
2025-05-17 09:14:09 -0500

    version 0.0000001

---
+++ mtg-collection-frontend/src/views/DeckDetailPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Deck Details</h1>
+    <p>Details for deck ID: {{ deckId }} will go here.</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  deckId: {
+    type: [String, Number],
+    required: true,
+  },
+});
+// Add script logic later
+</script>
+
+<style scoped>
+/* Add styles later */
+</style>
---
+++ mtg-collection-frontend/src/views/DeckListPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>My Decks</h1>
+    <p>Deck listing will go here.</p>
+  </div>
+</template>
+
+<script setup>
+// Add script logic later
+</script>
+
+<style scoped>
+/* Add styles later */
+</style>
